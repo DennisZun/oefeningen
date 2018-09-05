6209ddb49c080c67424c4d91c9582503446bbb19
dennisz <d@thuis.adfa>
2018-09-05 16:59:39 +0200

    oefening

---
+++ src/com/dennis/oefeningen/chap11exceptions/MusicTest1.java
@@ -0,0 +1,16 @@
+package com.dennis.oefeningen.chap11exceptions;
+
+import javax.sound.midi.*;
+
+public class MusicTest1 {
+
+    public void play() {
+        Sequencer sequencer = MidiSystem.getSequencer();
+        System.out.println("We got a sequencer");
+    }
+
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
---
+++ src/com/dennis/oefeningen/chap11exceptions/helloWorldAug2018.java
@@ -0,0 +1,8 @@
+package com.dennis.oefeningen.chap11exceptions;
+
+public class helloWorldAug2018 {
+
+    public static void main(String[] args) {
+        System.out.println("testaug2018");
+    }
+}
